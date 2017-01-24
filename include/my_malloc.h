d3a74af3c0ce429e196d5e50a763aac6349e5441
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-01-24 14:04:51 +0100

    ok

--- include/my_malloc.h
+++ include/my_malloc.h
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue Jan 24 13:19:13 2017 arnaud.alies
-** Last update Tue Jan 24 13:59:50 2017 arnaud.alies
+** Last update Tue Jan 24 14:04:39 2017 arnaud.alies
 */
 
 #ifndef MY_MALLOC_H_
