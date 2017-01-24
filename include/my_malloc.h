9921af7a1a16e6ee748a81820a3c9fd045c4f95d
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-01-24 14:00:06 +0100

    k

--- include/my_malloc.h
+++ include/my_malloc.h
@@ -5,11 +5,13 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue Jan 24 13:19:13 2017 arnaud.alies
-** Last update Tue Jan 24 13:19:58 2017 arnaud.alies
+** Last update Tue Jan 24 13:59:50 2017 arnaud.alies
 */
 
 #ifndef MY_MALLOC_H_
 #define MY_MALLOC_H_
+
+void show_alloc_mem()
 
 void *malloc(size_t size);
 void *realloc(void *ptr, size_t size);
