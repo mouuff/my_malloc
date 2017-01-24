672519b5a6ea607dbe6c8a73db5efbaa28a210ac
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-01-24 13:28:38 +0100

    ok

--- src/malloc.c
+++ src/malloc.c
@@ -5,8 +5,10 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue Jan 24 13:27:39 2017 arnaud.alies
-** Last update Tue Jan 24 13:28:00 2017 arnaud.alies
+** Last update Tue Jan 24 13:28:29 2017 arnaud.alies
 */
+
+#include <stdlib.h>
 
 void *malloc(size_t size)
 {
