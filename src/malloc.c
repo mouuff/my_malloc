b932e46607ed4147a08e90965c73b23a9cbc30c2
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-01-24 14:57:51 +0100

    ok

--- src/malloc.c
+++ src/malloc.c
@@ -5,12 +5,20 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue Jan 24 13:27:39 2017 arnaud.alies
-** Last update Tue Jan 24 14:45:53 2017 arnaud.alies
+** Last update Tue Jan 24 14:57:27 2017 arnaud.alies
 */
 
+#include <unistd.h>
 #include "my_malloc.h"
 
-void *malloc(size_t size)
+void		*malloc(size_t size)
 {
-  return (NULL);
+  t_alloc	*alloc;
+  void		*res;
+
+  res = sbrk(size + sizeof(t_alloc));
+  alloc = res;
+  alloc->magic = MAGIC;
+  alloc->size = size;
+  return (res + sizeof(t_alloc));
 }
