579bdc53bf7f713f889464d863d1172f242a8688
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-01-31 10:59:04 +0100

    ok

--- src/malloc.c
+++ src/malloc.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 **
 ** Started on  Tue Jan 24 13:27:39 2017 arnaud.alies
-** Last update Fri Jan 27 13:03:58 2017 arnaud.alies
+** Last update Tue Jan 31 10:55:44 2017 arnaud.alies
 */
 
 #include <string.h>
@@ -15,6 +15,18 @@
 t_chunk		*g_start = NULL;
 t_chunk		*g_prev = NULL;
 pthread_mutex_t	g_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+t_chunk		*init_node(void *pos, size_t size, int used)
+{
+  t_chunk	*alloc;
+
+  alloc = pos;
+  alloc->magic = MAGIC;
+  alloc->size = size;
+  alloc->used = used;
+  alloc->next = NULL;
+  return (alloc);
+}
 
 void		*malloc(size_t size)
 {
@@ -35,10 +47,7 @@ void		*malloc(size_t size)
     }
   if (g_start == NULL)
     g_start = alloc;
-  alloc->magic = MAGIC;
-  alloc->size = size;
-  alloc->used = 1;
-  alloc->next = NULL;
+  alloc = init_node(alloc, size, 1);
   if (g_prev != NULL)
     g_prev->next = alloc;
   g_prev = alloc;
