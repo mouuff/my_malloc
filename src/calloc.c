0edf4f10b6ce8e326fd84fe2693d4041d9ebae40
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-02-01 17:50:00 +0100

    ok

--- src/calloc.c
+++ src/calloc.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Wed Jan 25 10:28:25 2017 arnaud.alies
-** Last update Thu Jan 26 15:59:33 2017 arnaud.alies
+** Last update Wed Feb  1 17:48:50 2017 arnaud.alies
 */
 
 #include <string.h>
@@ -17,6 +17,6 @@ void            *calloc(size_t nmemb, size_t size)
 
   size *= nmemb;
   if ((new = malloc(size)) != NULL)
-    memset(new, 0, size);
+    memset(new, '\0', size);
   return (new);
 }
