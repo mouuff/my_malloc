e39b960420775d93df36b5560a3b37919c8d347b
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-02-09 14:48:42 +0100

    ok

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue Jan 24 16:54:55 2017 arnaud.alies
-** Last update Fri Jan 27 12:00:33 2017 arnaud.alies
+** Last update Thu Feb  9 14:48:36 2017 arnaud.alies
 */
 
 #include <assert.h>
@@ -14,21 +14,15 @@
 #include <stdlib.h>
 #include "./include/my_malloc.h"
 
-
 int	main()
 {
   char *truc = malloc(1000);
   char *hue = malloc(300);
   int *array;
-  
+
   free(truc);
-
   array = calloc(sizeof(int), 30);
-
   memset(hue, 0, 300);
-
   show_alloc_mem();
-
-
   return (0);
 }
