c9100949dfbbdd57d180c45f1dfcdbb9610068a2
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-02-09 14:47:56 +0100

    ok

--- src/free.c
+++ src/free.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 **
 ** Started on  Tue Jan 24 16:09:56 2017 arnaud.alies
-** Last update Tue Feb  7 16:45:27 2017 arnaud.alies
+** Last update Thu Feb  9 14:47:51 2017 arnaud.alies
 */
 
 #include "my_malloc.h"
@@ -36,6 +36,6 @@ void		free(void *ptr)
       return ;
     }
   alloc->used = 0;
-  free_regroup(alloc);
+  //free_regroup(alloc);
   pthread_mutex_unlock(&g_mutex);
 }
