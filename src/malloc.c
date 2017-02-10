d07ccebe7561c55f72ede42545da8ef4ed68a1b6
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-02-10 14:33:54 +0100

    ok

--- src/malloc.c
+++ src/malloc.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 **
 ** Started on  Tue Jan 24 13:27:39 2017 arnaud.alies
-** Last update Fri Feb 10 14:26:38 2017 arnaud.alies
+** Last update Fri Feb 10 14:31:56 2017 arnaud.alies
 */
 
 #include <string.h>
@@ -16,21 +16,18 @@ t_chunk		*g_start = NULL;
 t_chunk		*g_prev = NULL;
 pthread_mutex_t	g_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void		*malloc(size_t size)
+static void	*malloc_chunk(size_t size)
 {
   t_chunk	*ptr;
 
   if (((intptr_t)size) < 0)
     return (NULL);
-  pthread_mutex_lock(&g_mutex);
   if ((ptr = chunk_reuse(size)) != NULL)
     {
-      pthread_mutex_unlock(&g_mutex);
       return (ptr + 1);
     }
   if ((ptr = sbrk(size + sizeof(t_chunk))) == (void*)-1)
     {
-      pthread_mutex_unlock(&g_mutex);
       return (NULL);
     }
   ptr->size = size;
@@ -42,6 +39,15 @@ void		*malloc(size_t size)
   if (g_prev != NULL)
     g_prev->next = ptr;
   g_prev = ptr;
-  pthread_mutex_unlock(&g_mutex);
   return (ptr + 1);
+}
+
+void	*malloc(size_t size)
+{
+  void	*res;
+
+  pthread_mutex_lock(&g_mutex);
+  res = malloc_chunk(size);
+  pthread_mutex_unlock(&g_mutex);
+  return (res);
 }
